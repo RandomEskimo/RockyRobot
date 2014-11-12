d3cda920412846faa9781b476713a3ae7dd86a17
RandomEskimo <randomeskimo42@gmail.com>
2014-11-12 11:18:42 +1100

    Added logging to the debug snippet

--- Snippets/DebugSnippet.php
+++ Snippets/DebugSnippet.php
@@ -1,17 +1,40 @@
 <?php
     
+    $locator->inc("TableSnippet");
+    
     class DebugSnippet extends Snippet
     {
         private static $info = array();
+        private static $log_text = array();
         
         public static function addInfo($info_name, $info)
         {
             self::$info[$info_name] = $info;
         }
         
+        public static function log($tag, $text)
+        {
+            self::$log_text[] = array($tag, $text);
+        }
+        
         public function genContent()
         {
             echo '<div class="debug" style="color:black;background-color:white;"><h3>Debug Info</h3>';
+            if(!empty(self::$log_text))
+            {
+                echo '<h4>Log</h4>';
+                $table = new TableSnippet();
+                $table->addSnippet(new TextSnippet("Tag"));
+                $table->addSnippet(new TextSnippet("Text"));
+                $table->addRow();
+                foreach(self::$log_text as $row)
+                {
+                    $table->addSnippet(new TextSnippet($row[0]));
+                    $table->addSnippet(new TextSnippet($row[1]));
+                    $table->addRow();
+                }
+                $table->genContent();
+            }
             $keys = array_keys(self::$info);
             foreach($keys as $key)
             {

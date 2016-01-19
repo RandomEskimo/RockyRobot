42cd41f1499e8ec97a60eb7350c35df055a5ae0e
flare <flare@WebTestHost.webtesthost>
2016-01-19 21:54:56 +1100

    changed the Logger file name to use date time

--- Other/Logger.php
+++ Other/Logger.php
@@ -2,7 +2,33 @@
 
 class Logger
 {
+    private static $lines = array();
     
+    public static function Log($text)
+    {
+        if(DEBUG)
+        {
+            self::$lines[] = $text;
+        }
+    }
+    
+    public static function CreateLog()
+    {
+        if(DEBUG && !empty(self::$lines))
+        {
+            $text  = "======================================\n";
+            $text .= "Information\n";
+            $text .= "======================================\n";
+            $text .= "{info to be added}\n";
+            $text .= "======================================\n";
+            foreach(self::$lines as $line)
+            {
+                $text .= $line . "\n";
+            }
+            $file_name = $_SERVER['DOCUMENT_ROOT'] . "/Logging/" . date('m/d/Y-h:i:s a', time()) . "-" . $_SERVER['REMOTE_ADDR'] . ".log";
+            file_put_contents($file_name, $text);
+        }
+    }
 }
 
 ?>
